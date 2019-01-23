edf10455d301a98d4194067bd8ed439d481430fd
Konrad Bartczak <konradbartczak@onet.pl>
2019-01-23 09:00:12 +0100

    Initial commit

---
+++ src/main/java/com/konrad/garagev3/Garagev3Application.java
@@ -0,0 +1,14 @@
+package com.konrad.garagev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Garagev3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Garagev3Application.class, args);
+    }
+
+}
+
