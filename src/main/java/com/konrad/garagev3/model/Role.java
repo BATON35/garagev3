70569abf643529fc01c7beddd03bfdc96c8f53b3
BATON35 <konradbartczak@zoho.eu>
2019-01-30 20:15:34 +0100

    Feature/model (#1)
    * Add models Role and User
    
    * Add DAO RoleRepository and UserRepository
    
    * Add UserService
    
    * Add security configuration
    
    * Add properties for DB
    
    * Add LoginController
    
    * In progress to remove
    
    * Add LoginController
    
    * Add images to index.html
    
    * Add registration page

---
+++ src/main/java/com/konrad/garagev3/model/Role.java
@@ -0,0 +1,25 @@
+package com.konrad.garagev3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+@Component
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+    @Column(name = "role")
+    private String role;
+}
---
+++ src/main/java/com/konrad/garagev3/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.konrad.garagev3.repository;
+
+import com.konrad.garagev3.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
