45761f01a7c341f8a7c142ffbc58ee26157ede02
Konrad Bartczak <konradbartczak@onet.pl>
2019-02-04 19:02:46 +0100

    Add show/hide switch to login and registration

--- src/main/java/com/konrad/garagev3/controller/LoginController.java
+++ src/main/java/com/konrad/garagev3/controller/LoginController.java
@@ -52,9 +52,11 @@ public class LoginController {
             userService.saveUser(user);
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
-            modelAndView.setViewName("registration");
+            modelAndView.setViewName("index");
 
         }
+
+
         return modelAndView;
     }
 
