a0c74f38bb72b4197bb60da3654ddaca654d0a4d
Konrad Bartczak <konradbartczak@onet.pl>
2019-02-07 22:37:25 +0100

    Add polish content.

--- src/main/java/com/konrad/garagev3/mail/MailDetails.java
+++ src/main/java/com/konrad/garagev3/mail/MailDetails.java
@@ -1,6 +1,6 @@
 package com.konrad.garagev3.mail;
 
 class MailDetails {
-    static final String MY_EMAIL = "onbordingwelcomeonboard@gmail.com";
-    static final String MY_PASSWORD = "onbording";
+    static final String MY_EMAIL = "twojwarsztatsamochodowy@gmail.com";
+    static final String MY_PASSWORD = "Aaaaaa84";
 }
