714727ea843f956bd3d5b4d206fa7ce0671e11a3
Konrad Bartczak <konradbartczak@onet.pl>
2019-06-06 13:24:50 +0200

    Add validation in ClientDto

--- src/main/java/com/konrad/garagev3/model/dto/ClientDto.java
+++ src/main/java/com/konrad/garagev3/model/dto/ClientDto.java
@@ -1,7 +1,18 @@
 package com.konrad.garagev3.model.dto;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 public class ClientDto {
+    @Length(min = 3, max = 127, message = "Pole imie musi musi składać sie z co najmniej trzech liter")
+    @NotEmpty(message = "Wypełnij pole imie klienta")
     private String name;
+    @Length(min = 3, max = 127, message = "Pole nazwisko musi musi składać sie z co najmniej trzech liter")
+    @NotEmpty(message = "Wypełnij pole nazwisko klienta")
+    private String surname;
+    @Email
     private String email;
     private String phoneNumber;
 }
