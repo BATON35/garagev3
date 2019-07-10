e3ed143074b17063a744a08b98a947c7fe334346
Konrad Bartczak <konradbartczak@onet.pl>
2019-07-10 14:14:12 +0200

    Add ClientServiceIT with docker database configuration

---
+++ src/test/java/com/konrad/garagev3/service/ClientServiceIT.java
@@ -0,0 +1,55 @@
+package com.konrad.garagev3.service;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MySQLContainer;
+
+import static com.konrad.garagev3.service.ClientServiceTestData.TEST_CLIENT_DTO;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(initializers = ClientServiceIT.Initializer.class)
+public class ClientServiceIT {
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of("spring.datasource.url = jdbc:mysql://"
+                            + mysql.getContainerIpAddress() + ":"
+                            + mysql.getMappedPort(3306) + "/"
+                            + mysql.getDatabaseName(),
+                    "spring.datasource.username = " + mysql.getUsername(),
+                    "spring.datasource.password = " + mysql.getPassword()).applyTo(configurableApplicationContext);
+        }
+    }
+
+    @Autowired
+    ClientService sut;
+
+    @ClassRule
+    public static MySQLContainer mysql = new MySQLContainer<>("mysql:5.7")
+            .withExposedPorts(3306);
+
+    public void fillDatabase(){
+        sut.saveClient(TEST_CLIENT_DTO);
+    }
+
+    @Test
+    public void clientNotExist() {
+        Assert.assertNull(sut.findClientByEmail("mailNotEsist@pl"));
+    }
+
+    @Test
+    public void clientExist() {
+      //  sut.findClientByEmail()
+    }
+}
